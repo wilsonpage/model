f247374fd6d11323ae679857af48d0a15ab6cf9b
SamGiles <aphro.cag@googlemail.com>
2013-08-16 16:45:28 +0100

    Behave more correctly when storing an array in the model and/or getting a key => value where key === 0

--- test/model.js
+++ test/model.js
@@ -11,6 +11,17 @@ buster.testCase('Model()', {
 		assert.equals(model.get('foo'), 'foo');
 		assert.equals(model.get('bar'), 'bar');
 		assert.equals(model.get('baz'), 'baz');
+	},
+	"Should store the array passed into the constructor": function() {
+		var model = new Model([
+        { foo: 'foo' },
+        { foo: 'bar' },
+        { foo: 'baz' }
+		]);
+
+		assert.equals(model.get(0).foo, 'foo');
+		assert.equals(model.get(1).foo, 'bar');
+		assert.equals(model.get(2).foo, 'baz');
 	}
 
 });
